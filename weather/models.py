28df959a47368a30fb68f66993e2ee5d27a3e32b
stanislav <stanislav.vasilchenko@yandex.ru>
2024-07-16 22:28:09 +0300

    create app weather

---
+++ weather/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# class WeatherRequest(models.Model):
+#     city = models.CharField(max_length=255, verbose_name='City')
+#     latitude = models.FloatField(verbose_name='Latitude', null=True, blank=True)
+#     longitude = models.FloatField(verbose_name='Longitude', null=True, blank=True)
