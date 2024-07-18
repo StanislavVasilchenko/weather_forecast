7afab876f8ba8cd683d33eb065b5e0252ae77808
stanislav <stanislav.vasilchenko@yandex.ru>
2024-07-18 21:42:44 +0300

    create view get_weather

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
+from weather.services import get_weather_with_site, get_coordinates
 
 
+def get_weather(request):
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        city_coordinates = get_coordinates(city)
+        if city_coordinates is None:
+            context_data = {'message': f"Город {city} не найден"}
+            return render(request, 'weather/weather_result.html', context_data)
+        context_data = get_weather_with_site(city_coordinates)
+        return render(request, 'weather/weather_result.html', context_data)
+    return render(request, 'weather/input_city_form.html')
