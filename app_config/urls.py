cabd27b5d95c952d4c80dfc38efb512642a3837b
stanislav <stanislav.vasilchenko@yandex.ru>
2024-07-19 18:12:59 +0300

    ref

--- app_config/urls.py
+++ app_config/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from app_config import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weather.urls', namespace='weather')),
