79b74eeee315040ba4a1f1e9367cd6d24a4959a3
stanislav <stanislav.vasilchenko@yandex.ru>
2024-07-19 18:11:00 +0300

    ref

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic import DetailView
 
 from weather.models import City
